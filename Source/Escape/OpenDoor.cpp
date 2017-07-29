120837f91f9acc53d3acc2c3be4d620f31f5a4a6
Jogui <jordicasa22@gmail.com>
2017-07-29 17:43:00 +0200

    basic room

--- Source/Escape/OpenDoor.cpp
+++ Source/Escape/OpenDoor.cpp
@@ -1,6 +1,7 @@
 // Copyright Jordi Casadevall Franco 2017
 
 #include "OpenDoor.h"
+#include "GameFramework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -19,8 +20,14 @@ void UOpenDoor::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	// Find the owning Actor
+	AActor* owner = GetOwner();
+
+	// Create a rotator
+	FRotator newRotation = FRotator(0.f, -60.f, 0.f);
+
+	// Set the door rotation
+	owner->SetActorRotation(newRotation);
 }
 
 
